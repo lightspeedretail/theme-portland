dd27e7fc89044edfd1a62409ba1938eccf712c4f
Kris Thom White <kris@xsilva.com>
2013-12-18 13:30:01 -0700

    WS-640 Portland theme is missing Order Lookup and Wish List search if the user is logged in

--- views/site/_search.php
+++ views/site/_search.php
@@ -36,8 +36,11 @@
 					}',
 			))); ?>
 	</span>
-	</form>
+	<?php echo CHtml::endForm(); ?>
+
 </div>
-<div class="span1">
+<div class="span1 right">
 	<span class="search_advanced right"><a href="<?php echo _xls_site_url('/search'); ?>"><img class="spyglass" src="<?= Yii::app()->theme->baseUrl; ?>/css/images/adv_search.png"></a></span>
+	<span class="search_advanced right"><a href="<?php echo _xls_site_url('/search'); ?>"><?php echo Yii::t('global','Advanced Search'); ?></a></span>
 </div>
+<br clear="both">
